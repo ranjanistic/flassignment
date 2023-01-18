ca81664f84ac9fa51396feb8779b37144e8818d0
ranjanistic <priyanshuranjan88@gmail.com>
2023-01-19 00:07:04 +0530

    update api host

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_APIHOST,
+    baseURL: "https://flassignment.onrender.com",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
